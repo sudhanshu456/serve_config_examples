f31b1672ce82fd3a95d87308cb10f1115b187f25
Sudhanshu Prajapati <spsupraj2d@gmail.com>
2024-06-24 22:50:30 +0530

    fix llm script

--- phi_llm/phi_llm.py
+++ phi_llm/phi_llm.py
@@ -24,13 +24,11 @@ logger = logging.getLogger("ray.serve")
 app = FastAPI()
 
 
-
-
 @serve.deployment(num_replicas=1)
 @serve.ingress(app)
 class APIIngress:
     def __init__(self, handle: DeploymentHandle) -> None:
-        self.openai_serving_chat = handle
+        self.handle = handle
 
     @app.post("/v1/chat/completions")
     async def create_chat_completion(
@@ -42,28 +40,20 @@ class APIIngress:
             - https://docs.vllm.ai/en/latest/serving/openai_compatible_server.html
         """
         logger.info(f"Request: {request}")
-        generator = await self.openai_serving_chat.create_chat_completion(
+        response = await self.handle.generate.remote(
             request, raw_request
         )
-        if isinstance(generator, ErrorResponse):
-            return JSONResponse(
-                content=generator.model_dump(), status_code=generator.code
-            )
-        if request.stream:
-            return StreamingResponse(content=generator, media_type="text/event-stream")
-        else:
-            assert isinstance(generator, ChatCompletionResponse)
-            return JSONResponse(content=generator.model_dump())
-
+        return response
+        
 
 @serve.deployment(
     ray_actor_options={"num_gpus": 1},
-    autoscaling_config={"min_replicas": 0, "max_replicas": 2},
+    autoscaling_config={"min_replicas": 0, "max_replicas": 1},
 )
 class VLLMDeployment:
     def __init__(self):
         engine_args = AsyncEngineArgs(
-        gpu_memory_utilization=0.8,
+        # gpu_memory_utilization=0.8,
         model="microsoft/Phi-3-mini-128k-instruct",
         tensor_parallel_size=1,
         )
@@ -79,5 +69,17 @@ class VLLMDeployment:
         self.openai_serving_chat = OpenAIServingChat(
             self.engine, served_model_names, response_role, lora_modules, chat_template
         )
+       
+    def generate(self,  request: ChatCompletionRequest, raw_request: Request):
+        generator = self.openai_serving_chat.create_chat_completion(request, raw_request)
+        if isinstance(generator, ErrorResponse):
+            return JSONResponse(
+                content=generator.model_dump(), status_code=generator.code
+            )
+        if request.stream:
+            return StreamingResponse(content=generator, media_type="text/event-stream")
+        else:
+            assert isinstance(generator, ChatCompletionResponse)
+            return JSONResponse(content=generator.model_dump())
 
 entrypoint = APIIngress.bind(VLLMDeployment.bind())
