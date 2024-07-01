9574119848eb841cc3babf743379770715a67c3b
Sudhanshu Prajapati <spsupraj2d@gmail.com>
2024-07-01 12:22:31 +0530

    use gpus

--- ray-serve/vllm_engine.py
+++ ray-serve/vllm_engine.py
@@ -61,7 +61,7 @@ def _prepare_engine_args():
 @serve.deployment(name='VLLMInference',
                   num_replicas=1,
                   max_concurrent_queries=10,
-                  ray_actor_options={"num_gpus": 0.0}
+                  ray_actor_options={"num_gpus": 1.0}
                   )
 @serve.ingress(app)
 class VLLMInference:
